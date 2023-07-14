bb66a94971c2d619095333c018d5f286284ff20a
Saqib Amin <saqibamin257@gmail.com>
2023-07-14 17:28:16 +0500

    CRUD Operation Department

---
+++ src/Employee.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export class Employee extends React.Component{
+    render(){
+        return(
+        <div>
+        <h3>This is Employee Page</h3>    
+        </div>);
+    }
+}
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export class Home extends React.Component{
+    render(){
+        return(
+        <div>
+        <h3>This is Home Page</h3>    
+        </div>);
+    }
+}
