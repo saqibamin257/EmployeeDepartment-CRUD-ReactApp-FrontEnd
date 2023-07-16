e6f6c052866a253a1739bb06799cafa38f3d478d
Saqib Amin <saqibamin257@gmail.com>
2023-07-16 10:25:42 +0500

    added employee component

--- src/Department.js
+++ src/Department.js
@@ -14,22 +14,24 @@ export class Department extends React.Component {
             DepartmentNameFilter:"",
             departmentsWithoutFilter:[]
         }
-    }
+    }   
+
     FilterFn(){
         var DepartmentIdFilter=this.state.DepartmentIdFilter;
         var DepartmentNameFilter = this.state.DepartmentNameFilter;
 
         var filteredData=this.state.departmentsWithoutFilter.filter(
             function(el){
-                return(el.id.toString().toLowerCase().includes(
+                return el.id.toString().toLowerCase().includes(
                     DepartmentIdFilter.toString().trim().toLowerCase()
                 )&&
                 el.departmentName.toString().toLowerCase().includes(
                     DepartmentNameFilter.toString().trim().toLowerCase()
-                )) 
+                )
             }
         );
         this.setState({departments:filteredData});
+        console.log(filteredData);
     }
 
     changeDepartmentIdFilter = (e)=>{
@@ -52,7 +54,6 @@ export class Department extends React.Component {
                 return (b[prop]>a[prop])?1:((b[prop]<a[prop])?-1:0);
             }
         });
-
         this.setState({departments:sortedData});
     }
 
@@ -171,14 +172,14 @@ export class Department extends React.Component {
                                         placeholder="Filter" />
 
                                     <button type="button" className="btn btn-light"
-                                        onClick={() => this.sortResult('DepartmentId', true)}>
+                                        onClick={() => this.sortResult('id', true)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
                                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z" />
                                         </svg>
                                     </button>
 
                                     <button type="button" className="btn btn-light"
-                                        onClick={() => this.sortResult('DepartmentId', false)}>
+                                        onClick={() => this.sortResult('id', false)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
                                             <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z" />
                                         </svg>
@@ -193,14 +194,14 @@ export class Department extends React.Component {
                                         placeholder="Filter" />
 
                                     <button type="button" className="btn btn-light"
-                                        onClick={() => this.sortResult('DepartmentName', true)}>
+                                        onClick={() => this.sortResult('departmentName', true)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
                                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z" />
                                         </svg>
                                     </button>
 
                                     <button type="button" className="btn btn-light"
-                                        onClick={() => this.sortResult('DepartmentName', false)}>
+                                        onClick={() => this.sortResult('departmentName', false)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
                                             <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z" />
                                         </svg>
@@ -242,6 +243,9 @@ export class Department extends React.Component {
                         )}
                     </tbody>
                 </table>
+
+                {/****  modal *****/}
+                
                 <div className="modal fade" id="exampleModal" tabIndex="-1" aria-hidden="true">
 <div className="modal-dialog modal-lg modal-dialog-centered">
 <div className="modal-content">
